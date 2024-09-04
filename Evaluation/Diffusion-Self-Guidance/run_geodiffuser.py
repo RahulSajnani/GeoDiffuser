09beeb5f60264e3d556038b6a32834328ce8a54c
RahulSajnani <rahulsajnani2010@hotmail.com>
2024-09-04 15:34:23 -0400

    Update env

---
+++ Evaluation/Diffusion-Self-Guidance/run_geodiffuser.py
@@ -0,0 +1,209 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:128"
+import torch
+from free_guidance import StableDiffusionFreeGuidancePipeline
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+import torchvision.transforms.functional as TF
+import torch.nn.functional as F
+from tqdm.auto import tqdm
+from torch.nn import init
+from utils.guidance_functions import *
+import argparse
+from diffusers import LMSDiscreteScheduler, DDPMScheduler, DDIMScheduler, DPMSolverMultistepScheduler, DDIMInverseScheduler, StableDiffusionPipeline
+from utils import *
+from PIL import Image
+from utils.geodiffuser_data_utils import *
+
+os.environ["HF_HOME"] = "/oscar/scratch/rsajnani/rsajnani/research/.cache/hf"
+
+
+
+
+def safety_checker(clip_input, images, **kwargs):
+
+    return images, [False]
+
+
+def get_image_latent(img_path, device="cuda"):
+
+    if img_path is None: return None
+    img = Image.open(img_path)
+    img = img.convert('RGB')
+    image = preprocess_image(img).to(device)
+    init_latents = pipe.vae.encode(image.half()).latent_dist.sample() * 0.18215
+
+    return init_latents
+
+
+def load_models(model_id = "CompVis/stable-diffusion-v1-4", device="cuda", variant="fp16"):
+
+    pipe = StableDiffusionFreeGuidancePipeline.from_pretrained(model_id, torch_dtype=torch.float16, variant=variant)
+    pipe.unet = UNetDistributedDataParallel(pipe.unet, device_ids=[0]).cuda()
+
+    pipe.unet = pipe.unet.module
+    pipe = pipe.to(device)
+    pipe.scheduler = DDIMScheduler.from_config(pipe.scheduler.config)
+    pipe.enable_attention_slicing()
+    torch.backends.cudnn.benchmark = True
+
+
+    sd_ori_pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16, variant=variant)
+    sd_ori_pipe.unet = UNetDistributedDataParallel(sd_ori_pipe.unet, device_ids=[0]).cuda()
+    sd_ori_pipe.unet = sd_ori_pipe.unet.module
+    sd_ori_pipe = sd_ori_pipe.to(device)
+    sd_ori_pipe.scheduler = DDIMScheduler.from_config(sd_ori_pipe.scheduler.config)
+    sd_ori_pipe.enable_attention_slicing()
+    torch.backends.cudnn.benchmark = True
+
+    sd_ori_pipe.safety_checker = safety_checker
+
+
+    pipe_denoising_scheduler = DDIMScheduler.from_config(pipe.scheduler.config)
+    pipe_noising_scheduler = DDIMInverseScheduler.from_config(pipe.scheduler.config)
+
+
+
+    return pipe, sd_ori_pipe, pipe_denoising_scheduler, pipe_noising_scheduler
+
+
+def run_self_guidance_folder(pipe, sd_ori_pipe, pipe_denoising_scheduler, pipe_noising_scheduler, seed, dataset_path):
+
+
+    print(f"[INFO]: Running for folder: {dataset_path}")
+
+    exp = read_exp(dataset_path)
+    print(exp.keys())
+    print(exp["prompt_txt"])
+    prompt = exp["prompt_txt"]
+    object_to_edit = prompt.split(" ")[-1]
+    objects = [object_to_edit]
+    print(prompt, object_to_edit, objects, exp["path_name"])
+    img_path = complete_path(exp["path_name"]) + "input_image.png"
+
+
+
+    image = exp["input_image_png"]
+    mask = exp["input_mask_png"]
+    depth = exp["depth_npy"]
+    transform_mat = exp["transform_npy"]
+    print(exp["prompt_txt"])
+    K = camera_matrix(550, 550, image.shape[1] / 2.0, image.shape[0] / 2.0)
+
+    t_w, t_h = get_geodiff_translation(image, K, depth, transform_mat, mask)
+    print(t_w, t_h, exp["path_name"])
+
+    
+
+
+
+    # seed = int(torch.rand((1,)) * 100000)
+    generator=torch.manual_seed(seed)
+
+    # prompt = 'a photo of a donut and a shot of espresso on a table'
+    # img_path = './img/donut.png'
+    latents = get_image_latent(img_path)   
+    print(latents.shape)
+
+
+
+
+    # Perform Inversion
+    sd_ori_pipe.scheduler = pipe_noising_scheduler
+    inv_latents, _ = sd_ori_pipe(prompt=prompt, negative_prompt="", guidance_scale=guidance_scale,
+                          output_type='latent', return_dict=False,
+                          num_inference_steps=50, latents=latents)
+
+    # # Reconstruct image
+    # sd_ori_pipe.scheduler = pipe_denoising_scheduler
+    # out = sd_ori_pipe(prompt=prompt, negative_prompt="", guidance_scale=guidance_scale,
+    #                     num_inference_steps=50, latents=inv_latents)
+
+
+
+
+    # Perform Edit using self guidance
+    pipe.scheduler = pipe_denoising_scheduler
+    
+    # Move instruction to move by shape
+    move = partial(roll_shape_2, mag_direction=(t_h, t_w))
+    # guidance losses and terms
+    guidance = partial(move_object_by_shape, shape_weight=0.5, appearance_weight=2.0, position_weight=8, tau=move)
+    # perform the edit using diffusion self guidance
+    image_list = pipe(prompt, obj_to_edit = object_to_edit, height=512, width=512, 
+                    num_inference_steps=50, generator=generator, latents=inv_latents,
+            max_guidance_iter_per_step=3, guidance_func=guidance, g_weight=100, guidance_scale=guidance_scale)
+
+
+    im_edit = np.asarray(image_list[0].images[0])
+    print(im_edit.min(), im_edit.max())
+
+    d_path = complete_path(exp["path_name"]) + "diffusion_self_guidance/"
+    os.makedirs(d_path, exist_ok=True)
+    im_edit = resize_image(im_edit, exp["image_shape_npy"])
+
+    plt.imsave(d_path + "result_diffusion_self_guidance.png", im_edit)
+
+    # for i, image in enumerate(image_list):
+    #     image.images[0].save(f"./{seed}_{i}.png")
+
+
+def run_self_guidance_on_geodiffuser_data(pipe, sd_ori_pipe, pipe_denoising_scheduler, pipe_noising_scheduler, seed, exp_root_folder):
+
+    folder_list = glob.glob(complete_path(exp_root_folder) + "**/")
+    folder_list.sort()
+
+    # print(folder_list)
+    # exit()
+
+    if check_if_exp_root(exp_root_folder):
+        root_folders_list = folder_list
+        for f in root_folders_list:
+            folder_list = glob.glob(complete_path(f) + "**/")
+            folder_list.sort()
+
+            exp_cat = f.split("/")[-2]
+            if not (exp_cat == "Translation_2D"):
+                continue
+
+            for exp_folder in folder_list:
+                run_self_guidance_folder(pipe, sd_ori_pipe, pipe_denoising_scheduler, pipe_noising_scheduler, seed, exp_folder)
+    
+            # exit()
+    else:
+        for exp_folder in folder_list:
+            run_self_guidance_folder(pipe, sd_ori_pipe, pipe_denoising_scheduler, pipe_noising_scheduler, seed, exp_folder)
+
+    pass
+
+if __name__=="__main__":
+
+    guidance_scale = 5.0
+
+    torch.cuda.manual_seed_all(1234) 
+    torch.set_printoptions(precision=2, linewidth=140, sci_mode=False)
+    print("Start Inference!")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_id', type=str, default="CompVis/stable-diffusion-v1-4")
+    parser.add_argument("--dataset_path", type=str, required=True)
+    parser.add_argument('--seed', type=int, default=12345)
+    args = parser.parse_args()
+
+    pipe, sd_ori_pipe, pipe_denoising_scheduler, pipe_noising_scheduler = load_models(args.model_id)
+
+
+    # run_self_guidance_on_geodiffuser_data(pipe, sd_ori_pipe, pipe_denoising_scheduler, pipe_noising_scheduler, seed=args.seed, exp_root_folder = args.dataset_path)
+
+
+    run_self_guidance_folder(pipe, sd_ori_pipe, pipe_denoising_scheduler, pipe_noising_scheduler, seed=args.seed, dataset_path = args.dataset_path)
+
+
+
+
+
+
+
+
+
---
+++ Evaluation/Diffusion-Self-Guidance/utils/geodiffuser_data_utils.py
@@ -0,0 +1,274 @@
+
+import numpy as np
+import cv2, glob, argparse, os
+import matplotlib.pyplot as plt
+import time
+import os
+os.environ['HF_HOME'] = "/oscar/scratch/rsajnani/rsajnani/research/.cache/hf"
+
+
+def count_folders(directory):
+    return len([name for name in os.listdir(directory) if os.path.isdir(os.path.join(directory, name))])
+
+def create_folder(directory):
+    os.makedirs(directory, exist_ok = True)
+
+def complete_path(directory):
+    return os.path.join(directory, "")
+
+
+def read_image(im_path):
+
+    im = plt.imread(im_path)
+    # print(im.min(), im.max(), im.shape, im_path)
+
+    if len(im.shape) == 3:
+        im = im[..., :3]
+    
+    if im.max() <= 1.0:
+        im = (im * 255.0).astype("uint8")
+    # print(im.min(), im.max(), im.shape, im_path)
+    return im
+
+
+def read_txt_file(f_path):
+
+    with open(f_path, "r") as f:
+        txt = f.read()
+
+    return txt
+
+def file_exists(f_path):
+    return os.path.exists(f_path)
+
+def read_exp(d_path):
+    save_folder = complete_path(d_path)
+
+
+    img_path = save_folder + "input_image.png"
+    depth_path = save_folder + "depth.npy"
+    mask_path = save_folder + "input_mask.png"
+    bg_path = save_folder + "background_image.png"
+    depth_vis_path = save_folder + "depth.png"
+    transform_path = save_folder + "transform.npy"
+    im_shape = save_folder + "image_shape.npy"
+    
+    transformed_image_path = save_folder + "transformed_image.png"
+    result_path = save_folder + "result.png"
+    result_ls_path = save_folder + "resized_result_ls.png"
+    zero123_result_path = save_folder + "zero123/lama_followed_by_zero123_result.png"
+    object_edit_result_path = save_folder + "object_edit/result_object_edit.png"
+    resized_input_image = save_folder + "resized_input_image_png.png"
+    resized_input_mask = save_folder + "resized_input_mask_png.png"
+
+    prompt_path = save_folder + "prompt.txt"
+    
+    all_paths = [img_path, depth_path, mask_path, bg_path, depth_vis_path, transform_path, transformed_image_path, result_path, im_shape, result_ls_path, zero123_result_path, resized_input_image, object_edit_result_path, resized_input_mask, prompt_path]
+    
+    out_dict = {}
+    for f_name in all_paths:
+        base_name = os.path.basename(f_name)
+        key_name = base_name.split(".")[0]
+        f_type = base_name.split(".")[1]
+        
+
+        if file_exists(f_name):
+            if f_type == "png":
+                out_dict[key_name + "_png"] = read_image(f_name)
+            elif f_type == "npy":
+                out_dict[key_name + "_npy"] = np.load(f_name)
+            elif f_type == "txt":
+                out_dict[key_name + "_txt"] = read_txt_file(f_name)
+        else:
+            out_dict[key_name + "_" + f_type] = None
+    if out_dict["image_shape_npy"] is None:
+        out_dict["image_shape_npy"] = np.array([512, 512])
+    out_dict["path_name"] = d_path
+    return out_dict
+
+
+def get_axis_points(scale = 1.0):
+
+    points = np.float32([[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 0, 0]]).reshape(-1, 3) * scale
+
+    return points
+
+# def project_points_to_3D(im, K, d):
+
+def translateMatrixFromVector(v):
+
+    translation_matrix = np.eye(4)
+    # translation_matrix[0,3] += x
+    # translation_matrix[1,3] += y
+    # translation_matrix[2,3] += z
+    translation_matrix[:3, 3] += v
+
+    return translation_matrix
+
+
+def project_points_to_3D(im, K, d):
+
+    h, w = np.indices(im[..., 0].shape)
+    cam_coords = np.stack([w, h, np.ones_like(h)], -1) # h, w, 3
+
+    # print(cam_coords.shape)
+    pts_3D = (np.linalg.inv(K)[None, None] @ cam_coords[..., None])[..., 0] * d[..., None] # h,, w, 3
+    return pts_3D, cam_coords
+
+def get_geodiff_translation(im, K, d_in, transform_mat, obj_mask):
+
+    d, mask_d = prepare_depth_for_projection(d_in, obj_mask[..., 0] / 255.0)
+
+
+    pts_3D, cam_coords = project_points_to_3D(im, K, d) # h, w, 3
+
+    pts_3D_pcd = np.reshape(pts_3D, (-1, 3))
+    obj_center = np.mean(pts_3D_pcd[np.reshape(mask_d, (-1)) >= 0.5, :], 0)
+
+    pts_3D_hom = np.concatenate([pts_3D, np.ones_like(pts_3D[..., :1])], -1) # h, w, 4
+
+    T_mean = translateMatrixFromVector(-obj_center)
+
+    pose_transform = T_mean @ transform_mat @ np.linalg.inv(T_mean)
+
+    transformed_coords = (pose_transform[None, None] @ pts_3D_hom[..., None])[..., 0] # h, w, 4, 1
+
+
+
+
+    projected_pts = (K[None, None] @ transformed_coords[:, :, :3, None])[..., 0]
+
+    projected_pts[:, :, :2] = (projected_pts[:, :, :2] / projected_pts[..., -1:])
+
+    deviation = np.mean((projected_pts[..., :2] - cam_coords[..., :2])[mask_d >= 0.5], 0)
+    
+
+    return deviation
+
+
+    # projected_pts = projected_pts[mask_d > 0.5]
+    # print(projected_pts.shape, projec)
+
+
+
+
+
+def draw_axis(img, R, t, K, points, shift = None):
+    # unit is mm
+    rotV, _ = cv2.Rodrigues(R)
+
+    axisPoints, _ = cv2.projectPoints(points, rotV, t, K, (0, 0, 0, 0))
+    # axisPoints = axi
+    # print(axisPoints[-1])
+
+    if shift is not None:
+        
+        w_i, h_i = axisPoints[-1].ravel()
+        # print(w_i)
+        h_new = h_i / shift[0]
+        w_new = w_i / shift[1]
+        shift = np.array([w_new - w_i, h_new - h_i])
+        # print(shift)
+        axisPoints = axisPoints + shift[None]
+    # print(axisPoints)
+    axisPoints = axisPoints.astype("int")
+    # axisPoints = np.concatenate(axisPoints[:, 1:], axisPoints[:, :1], -1)
+    # axisPoints = 
+    # print(axisPoints[-1].ravel(), axisPoints[1].ravel())
+    img = cv2.line(img, tuple(axisPoints[3].ravel()), tuple(axisPoints[2].ravel()), (0,0,255), 3)
+    img = cv2.line(img, tuple(axisPoints[3].ravel()), tuple(axisPoints[1].ravel()), (0,255,0), 3)
+    img = cv2.line(img, tuple(axisPoints[3].ravel()), tuple(axisPoints[0].ravel()), (255,0,0), 3)
+    return img
+
+def get_mask_center(mask):
+
+    m = mask.copy()
+    if m.max() > 1:
+        m = m / 255.0
+    if len(m.shape) > 2:
+        m = m[..., 0]
+
+    h_ind, w_ind = np.indices(m.shape)
+    h_mean = h_ind[m > 0.5].mean()
+    w_mean = w_ind[m > 0.5].mean()
+
+    return w_mean, h_mean
+
+def prepare_depth_for_projection(d, mask_in = None):
+
+    depth = d.copy()
+    if mask_in is not None:
+        obj_mask = mask_in
+
+        if len(mask_in.shape) > 2:
+            obj_mask = obj_mask[:, :, 0]
+        if obj_mask.max() > 1.0:
+            obj_mask = obj_mask / 255.0
+    else:
+        obj_mask = None
+        
+    if np.sum(depth) == 0.5 * (depth.shape[0] * depth.shape[1]):
+        depth = np.ones_like(depth) * 0.5
+
+    else:
+        # Normalize depth
+        depth = depth / (depth.max() + 1e-8)
+
+        depth[depth > 0.95] = 1000.0
+
+    mask = (depth < 100.0) * 1.0
+
+    if obj_mask is not None:
+        mask = obj_mask * mask
+
+
+    return depth, mask
+
+def get_exp_types():
+    
+    exp_types = ["Removal", "Rotation_3D", "Rotation_2D", "Translation_3D", "Scaling", "Mix", "Translation_2D"]
+
+    return exp_types
+
+def check_if_exp_root(exp_root_folder, folder_list = None):
+
+    if folder_list is None:    
+        folder_list = glob.glob(complete_path(exp_root_folder) + "**/")
+    
+    exp_types = get_exp_types()
+
+
+    for f in folder_list:
+        # print(f.split("/"))
+        if f.split("/")[-2] in exp_types:
+            return True
+
+    return False
+
+def camera_matrix(focal_x, focal_y, c_x, c_y):
+    '''
+    Constructs camera matrix
+    '''
+
+    K = np.array([[ focal_x,      0,     c_x],
+                  [     0,  focal_y,     c_y],
+                  [     0,      0,       1]])
+
+    return K
+
+
+def resize_image(image, aspect_ratio):
+
+    # h, w = image.shape[:2]
+    ratio = aspect_ratio[1] / aspect_ratio[0]
+    h, w = 512, 512
+
+    if ratio < 1:
+        new_h, new_w = h / ratio, w
+    else:
+        new_h, new_w = h, ratio * w
+
+    img = cv2.resize(image, (int(new_w),int(new_h)))
+
+    # input_img = np.array(Image.fromarray(img).resize((w, h), Image.NEAREST))
+    return img
